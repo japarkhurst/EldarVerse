e1820eabebf8ad5cc53d198e868a4633c7fee3a8
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-09-04 22:33:12 -0400

    Update B.py

--- 2025_09/B.py
+++ 2025_09/B.py
@@ -10,31 +10,57 @@ for i,row in enumerate(input.split('\n')):
     N,K = int(N),int(K)
     cases.append((N,K))
 
-
-for i,(N,K) in enumerate(cases,1):
-    cnt = N * (N-1)
-    print(f'Games: {cnt}')
+solutions = []
+for case,(N,K) in enumerate(cases,1):
+    gameCount = N * (N-1)
+    print(f'Games: {gameCount}')
     games = []
     for H in range(1,N+1):
         for A in range(1,N+1):
             if H != A:
                 games.append((H,A))
-    print(games)
-gameCount = cnt
-current = [[W]*gameCount]
-cumulative = []
-for i in range(N):
-    print(i)
-    print(current)
+    #print(games)
+    current = [[W]*gameCount]
+    cumulative = []
+    for i in range(len(games)):
+        #print(i)
+        #print(current)
+        cumulative += current
+        current = cumulative[::]
+        #print(cumulative)
+        new_results = []
+        for j in range(len(current)):
+            result = current[j][::]
+            result[i] = L
+            new_results.append(result)
+            #print(f'{result=}')
+        #print(len(new_results))
+        current = new_results
+        #print(current)
     cumulative += current
-    current = cumulative[::]
-    print(cumulative)
-    new_results = []
-    for j in range(len(current)):
-        result = current[j][::]
-        result[i] = L
-        new_results.append(result)
-        print(f'{result=}')
-    current = new_results
-    print(current)
+    #print(cumulative)
+    #print(len(cumulative))
     
+    sparseCount = 0
+    for result in cumulative:
+        resultDict = dict(zip(games,result))
+        #print(resultDict)
+        scores = []
+        maxScore = 0
+        minScore = 1000
+        for team in range(1,N+1):
+            homeWins = len([R for (H,A),R in resultDict.items() if R==W and H==team])
+            awayWins = len([R for (H,A),R in resultDict.items() if R==L and A==team])
+            score = homeWins + awayWins
+            maxScore = max(score,maxScore)
+            minScore = min(score,minScore)
+            scores.append((team,score))
+        rankings = sorted(scores,key=lambda x:x[1],reverse=True)
+        #print(rankings)
+        if maxScore-minScore > K:
+            sparseCount+=1
+    print(f'{sparseCount=}')
+    solutions.append(f'Case #{case}: {sparseCount}')
+    
+print('\n\n')
+print("\n".join(solutions))
