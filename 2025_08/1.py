8c855f24cccdd83cd4e8d57501a098d22c6b8264
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-09-06 10:28:52 -0400

    Create 2025_08/1.py

---
+++ 2025_08/1.py
@@ -0,0 +1,13 @@
+input = '''10
+Ftq gzqjmyuzqp xurq ue zaf iadft xuhuzs
+Znk cnurk oy mxkgzkx zngt znk yas ul ozy vgxzy
+Je ru eh dej je ru jxqj yi jxu gkuijyed
+Imagination is more important than knowledge
+R jv xwln jpjrw jbtrwp oxa hxda orwjwlrju bdyyxac
+Ftqdq ue za oxagp uf ue vgef eayqazq qxeqe oaybgfqd
+Vhmsdq hr bnlhmf
+Wvm lwma vwb aquxtg eits qvbw uwzlwz
+Kyzj zj wzev
+K vjkpm vjgtghqtg K ogog'''
+
+for i,line in enumerate(input.split('\n')):
