b3e8f8a97d7c646c58b5704b070e63351942a710
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-09-06 22:31:11 -0400

    Update I.py

--- 2025_09/I.py
+++ 2025_09/I.py
@@ -1,5 +1,23 @@
-print(bool("abc" < "abd"))
-print(bool("abc" < "ba"))
-print(bool("abc" < "abcd"))
-print(bool("abc" > "aaa"))
-print(bool("abc" > "a"))
+input = '''1
+4 6
+gojira
+rachvela
+lekva
+samurai
+ADD gojira rachvela
+ADD gojira lekva
+ADD lekva samurai
+SUGGEST samurai
+ADD samurai gojira
+SUGGEST samurai'''
+
+from collections import defaultdict
+friends = defaultdict(set())
+for i,row in enumerate(input.split('\n')):
+    if i < 1:
+        continue
+    elif row[0] == 'A':
+        _,f1,f2 = row.split(' ')
+        friends[f1].add(f2)
+        friends[f2].add(f1)
+        
