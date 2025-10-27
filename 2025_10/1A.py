59d2424c5175927477d401804036173fb1cb05f5
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-10-27 13:01:35 -0400

    Update 1A.py

--- 2025_10/1A.py
+++ 2025_10/1A.py
@@ -1,7 +1,28 @@
-for i,name in enumerate(input.split('\n')):
+input = '''4
+7 158
+10 777
+12 012345
+1 235'''
+
+results = []
+for i,row in enumerate(input.split('\n')):
     if i == 0:
         continue
-    count = len(set(name.lower()))
-    discount = .05*count
-    percent = int(round((1-discount)*100))
-    print(f'Case #{i}: {percent}')
+    nights,sequence = row.split(' ')
+    clans = list(sequence)
+    clanCount = len(clans)
+    countDict = {c:0 for c in clans}
+    n = 0
+    clanIndex = 0
+    while n < int(nights):
+        n+=1
+        clan = clans[clanIndex]
+        clanIndex+=1
+        if clanIndex >= clanCount:
+            clanIndex = 0
+        countDict[clan]+=1
+    #print(countDict)
+    counts = countDict.values()
+    results.append(f'Case #{i}: {max(counts)} {min(counts)}')
+    
+print("\n".join(results))
