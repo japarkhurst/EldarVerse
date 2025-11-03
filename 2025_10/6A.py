b13ebe0f30d68abfca3cd93e287e11ee1b3efb4f
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-02 22:06:29 -0500

    Update 6A.py

--- 2025_10/6A.py
+++ 2025_10/6A.py
@@ -1,4 +1,4 @@
-txt = '''1
+input = '''1
 #|,  .-.  .-.  ,|#
 ####`--------`####
 #(_     ^^     _)#
@@ -14,3 +14,17 @@ $##.-"  "  "'-.#$#
 ##\__|IIIIII|__/##
 ====='''
 
+results = []
+case = 1
+for i,row in enumerate(input.split('\n')):
+    if i == 0:
+        continue
+    lIndex = row.index('>')
+    rIndex = row.index('<')
+    if lIndex and rIndex:
+        token = row[lIndex+1:rIndex]
+    
+        results.append(f'Case #{case}: {token}')
+        case+=1
+
+print("\n".join(results))
